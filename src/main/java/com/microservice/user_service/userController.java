4036decb4c521cb6d70b4c67f57be02694a5134d
hbinmadhi <hbinmadhi@elm.sa>
2022-11-17 16:27:59 +0300

    add Cross

--- src/main/java/com/microservice/user_service/userController.java
+++ src/main/java/com/microservice/user_service/userController.java
@@ -2,18 +2,20 @@ package com.microservice.user_service;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/users")
+@CrossOrigin()
 public class userController {
 
     @GetMapping("/")
     public ResponseEntity<UserInformation> getUser(){
-        UserInformation userInformation = new UserInformation(1L,"hana",
-                "0500654379","Riyadh","Read");
+        UserInformation userInformation = new UserInformation(1L,"احمد",
+                "050545453","الرياض","قراءه");
         return new ResponseEntity<>(userInformation, HttpStatus.ACCEPTED);
     }
 }
